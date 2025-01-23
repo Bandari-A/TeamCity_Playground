fecf9a85ad2dcebb80f1443118dce7270aa46e6f
akhil <akhilbandari1@gmail.com>
2025-01-23 23:10:08 +0100

    Add newton soft Json

--- SimpleConsoleApp/Program.cs
+++ SimpleConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace SimpleConsoleApp
 {
+    using Newtonsoft.Json;
     using System;
 
     internal class Program
@@ -7,6 +8,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Print out");
+            var result = JsonConvert.SerializeObject(new object[] { });
         }
     }
 }
